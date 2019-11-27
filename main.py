8c8b028b74d9923e8a01d78873fd493581d0357b
joieeemithun77 <joieeemithun98@gmail.com>
2019-11-28 01:35:07 +0530

    changed main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print ("hii hello world :")
+def fun():
+
+    a = input("enter the father name : ")
+
+    b = input("enter the mother name : ")
+    c =(len(a))
+    d =(len(b))
+    if(c>d):
+        print("",a)
+    elif(c==d):
+        print("",a)
+        print("",b)
+    else:
+        print("",b)
+fun()
